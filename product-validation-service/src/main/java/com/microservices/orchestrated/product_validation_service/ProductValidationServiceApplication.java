5778c8aecc8dcbc655ac024155bc4f651736bff3
Fábio de Andrade <fabioac71@gmail.com>
2024-06-19 14:03:24 -0300

    changes

--- product-validation-service/src/main/java/com/microservices/orchestrated/product_validation_service/ProductValidationServiceApplication.java
+++ product-validation-service/src/main/java/com/microservices/orchestrated/product_validation_service/ProductValidationServiceApplication.java
@@ -2,9 +2,8 @@ package com.microservices.orchestrated.product_validation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ProductValidationServiceApplication {
 
 	public static void main(String[] args) {
